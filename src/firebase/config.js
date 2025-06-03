197409cbec2ec5ec19b587b61eff4bb96e1e1f2b
Sushma Shankar <sshan854@uw.edu>
2025-06-02 19:53:36 -0500

    protected api

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,16 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBi9BzPlq2KN9-E9qG6RYfAU2ZMp1d7BK4",
-    authDomain: "cafe-quiet.firebaseapp.com",
-    projectId: "cafe-quiet",
-    storageBucket: "cafe-quiet.firebasestorage.app",
-    messagingSenderId: "333469767219",
-    appId: "1:333469767219:web:bcd4eb29ce4464bc113546",
-    measurementId: "G-Q3CF0N3LH3",
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-};
+    projectId: "cafe-quiet",
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};``
   
 
 export const app = initializeApp(firebaseConfig);
